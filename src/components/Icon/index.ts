a99c759dcd537a191343088830429275bf178220
boysers <boyser2015@gmail.com>
2023-11-11 02:26:45 +0100

    feat(icon): add icons

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -5,3 +5,11 @@ export { SwimmingIcon } from './SwimmingIcon'
 export { BikeIcon } from './BikeIcon'
 
 export { AltersIcon } from './AltersIcon'
+
+export { FireIcon } from './FireIcon'
+
+export { MeatIcon } from './MeatIcon'
+
+export { AppleIcon } from './AppleIcon'
+
+export { BurgerIcon } from './BurgerIcon'
