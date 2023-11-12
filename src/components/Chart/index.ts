9dccb4a84f0d55c006a0e2ae55dedac8cff510b6
boysers <50039110+boysers@users.noreply.github.com>
2023-11-12 04:06:15 +0100

    fix: delete folders

--- src/components/Chart/index.ts
+++
@@ -1,7 +0,0 @@
-export { UserActivityBarChart } from './UserActivityBarChart'
-
-export { UserAvgSessionsLineChart } from './UserAvgSessionsLineChart'
-
-export { UserPerformanceRadarChart } from './UserPerformanceRadarChart'
-
-export { UserAvgScoreRadialBarChart } from './UserAvgScoreRadialBarChart'
